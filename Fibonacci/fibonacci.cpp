64a34aad37a05f0010eb6d73a0234b60344e34e4
ducanpro <annguyen0203006@gmail.com>
2026-01-02 00:01:11 +0700

    Implement Fibonacci sequence in C++

---
+++ Fibonacci/fibonacci.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void fibo(int n){
+	long long f[n+1];
+	f[0] = 0;
+	f[1] = 1;
+	 
+	for(int i = 2; i <= n; i++){
+		f[i] = f[i - 1] + f[i - 2];
+	}
+	
+	printf("Day Fibonacci: ");
+	for(int i = 0; i <= n; i++){
+		printf("%lld ", f[i]);
+	}
+}
+int main(){
+	int n;
+	printf("Nhap n: ");
+	scanf("%d", &n);
+	fibo(n);
+	return 0;
+}
