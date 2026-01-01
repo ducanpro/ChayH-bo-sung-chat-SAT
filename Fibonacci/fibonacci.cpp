53e0c96339077cedbb449479f713523a11d5a8be
ducanpro <annguyen0203006@gmail.com>
2026-01-02 00:42:16 +0700

    Update fibonacci.cpp

--- Fibonacci/fibonacci.cpp
+++ Fibonacci/fibonacci.cpp
@@ -3,8 +3,10 @@
 void fibo(int n){
 	long long f[n+1];
 	f[0] = 0;
+	//Nhap 0 là lấn chiếm lòng lề đường đấy
+	if{n >= 1){
 	f[1] = 1;
-	 
+	}
 	for(int i = 2; i <= n; i++){
 		f[i] = f[i - 1] + f[i - 2];
 	}
